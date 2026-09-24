e483f07dca2b6987de829c910cc688170f6ed5fe
Neeraj <neerajsharma977376@gmail.com>
2026-09-24 17:19:53 +0530

    go

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: "127.0.0.1",
+    host: true,
     port: 5173,
-    strictPort: false,
+    strictPort: true,
   },
   preview: {
-    host: "127.0.0.1",
+    host: true,
     port: 4173,
   },
 });
